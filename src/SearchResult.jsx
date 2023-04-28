bf3249444726c2991fa6505d0eced1406135021f
ConnorMcGehee <connorjames050@gmail.com>
2023-04-27 20:46:47 -0500

    Updated result format

--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -1,13 +1,17 @@
-function SearchResult({ createdAt, author, title, url, points, numComments, commentsUrl, authorUrl }) {
+function SearchResult({ createdAt, author, title, url, points, numComments }) {
+    const now = new Date();
+    const createdAtDate = new Date(createdAt);
+    const yearsAgo = now.getFullYear() - createdAtDate.getFullYear();
     return (
-        <>
-            <h2>{title}</h2>
-            <a href={url}>{url}</a>
-            <a href={commentsUrl}>{points} points</a>
-            <a href={authorUrl}>{author}</a>
-            <a href={commentsUrl}>{new Date(createdAt).toLocaleDateString()}</a>
-            <a href={commentsUrl}>{numComments} comments</a>
-        </>
+        <div className="result">
+            <h2><a href={url}>{title}</a></h2>
+            <ul className="resultProps">
+                <li className="resultProp">{points} points</li> |
+                <li className="resultProp">{author}</li> |
+                <li className="resultProp">{yearsAgo} years ago</li> |
+                <li className="resultProp">{numComments} comments</li>
+            </ul>
+        </div>
     );
 }
 
