d30ec560be9175134d21d5b5b9763316f46d396b
Ben Ballard <benb971@gmail.com>
2023-04-28 19:12:38 -0500

    List articles functionality, major css overhaul

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,18 +1,36 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faMessage } from '@fortawesome/free-regular-svg-icons'
+
 function SearchResult({ createdAt, author, title, url, points, numComments }) {
+
+    const upVote = <FontAwesomeIcon icon={faArrowUp} />
+    const downVote = <FontAwesomeIcon icon={faArrowDown} />
+    const comment = <FontAwesomeIcon icon={faMessage} />
+
     const now = new Date();
     const createdAtDate = new Date(createdAt);
     const yearsAgo = now.getFullYear() - createdAtDate.getFullYear();
 
     return (
-        <div className="result">
-            <h2><a href={url}>{title}</a></h2>
-            <ul className="resultProps">
-                <li className="resultProp">{points} points</li> |
-                <li className="resultProp">{author}</li> |
-                <li className="resultProp">{yearsAgo} years ago</li> |
-                <li className="resultProp">{numComments} comments</li>
-            </ul>
-        </div>
+        <a href={url}>
+            <div className="result">
+                <div className="pointBox">
+                    <button className='smIcon'>{upVote}</button>
+                    <h4>{points}</h4>
+                    <button className='smIcon'>{downVote}</button>
+                </div>
+                <div className="cardContent">
+                    <div className="sign">
+                        <h5 className="signProp">Posted by {author} {yearsAgo} years ago</h5>
+                        <h3 className="cardTitle">{title}</h3>
+                    </div>
+                    <button className="resultProp">{comment} {numComments} comments</button>
+                </div>
+            </div>
+        </a>
     );
 }
 
